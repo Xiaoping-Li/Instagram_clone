562d7a6e25b57028983069df0833fd66aff121fd
Xiaoping-Li <narci2008@gmail.com>
2019-04-13 14:24:19 -0700

    Add homescreen

--- InstagramClone/src/components/screens/index.js
+++ InstagramClone/src/components/screens/index.js
@@ -2,6 +2,7 @@ import WelcomeScreen from './WelcomeScreen';
 import SignInScreen from './SigninScreen';
 import SignUpScreen from './SignupScreen';
 import AuthLoadingScreen from './AuthLoadingScreen';
+import HomeScreen from './HomeScreen';
 import SearchScreen from './SearchScreen';
 import ProfileScreen from './ProfileScreen';
 import AddPostScreen from './AddPostScreen';
@@ -14,6 +15,7 @@ export {
     SignInScreen,
     SignUpScreen,
     AuthLoadingScreen,
+    HomeScreen,
     SearchScreen,
     ProfileScreen,
     AddPostScreen,
