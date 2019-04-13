570167b2b77899b4438570dc52bc5f2e3c5cad4e
Xiaoping-Li <narci2008@gmail.com>
2019-04-13 14:10:51 -0700

    AuthLoadingScreen

--- InstagramClone/src/components/screens/index.js
+++ InstagramClone/src/components/screens/index.js
@@ -1,10 +1,12 @@
 import WelcomeScreen from './WelcomeScreen';
 import SignInScreen from './SigninScreen';
 import SignUpScreen from './SignupScreen';
+import AuthLoadingScreen from './AuthLoadingScreen';
 
 
 export {
     WelcomeScreen,
     SignInScreen,
     SignUpScreen,
+    AuthLoadingScreen,
 }
