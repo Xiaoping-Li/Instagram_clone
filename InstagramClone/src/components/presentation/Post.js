390b5fd8ccdfbdc25301f74860d61ba832bd5723
Xiaoping-Li <narci2008@gmail.com>
2019-04-15 12:13:46 -0700

    Home page basic style is done

--- InstagramClone/src/components/presentation/Post.js
+++ InstagramClone/src/components/presentation/Post.js
@@ -7,38 +7,51 @@ import {
 } from 'react-native';
 
 import Icon from '@expo/vector-icons/Feather';
+import EllipsisIcon from '@expo/vector-icons/AntDesign';
 
 class Post extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <View>
-                    <Image style={{width: 50, height: 50}} source={{uri: this.props.post.thumbnail}} />
-                    <Text>{this.props.post.user}</Text>
+                <View style={styles.header}>
+                    <Image style={{width: 40, height: 40, margin: 10, borderRadius: 20,}} source={{uri: this.props.post.thumbnail}} />
+                    <Text style={{fontSize: 15, fontWeight: 'bold',}}>{this.props.post.user}</Text>
+                    <EllipsisIcon 
+                        style={{ marginLeft: 200,}}
+                        onPress={() => {}} 
+                        name="ellipsis1" 
+                        size={25}/>
                 </View>
-                <Image style={{width: 250, height: 250}} source={{uri: this.props.post.uri}} />
-                <View>
+
+                <Image 
+                    source={{uri: this.props.post.uri}}
+                    style={{height: 300, width: null, flex: 1,}}
+                />
+               
+                <View style={styles.icons}>
                     <Icon
                         style={{ paddingLeft: 10}}
                         onPress={() => {}} 
                         name="heart" 
-                        size={30} 
+                        size={20} 
                     />
 
                     <Icon
                         style={{ paddingLeft: 10}}
                         onPress={() => {}} 
                         name="message-circle" 
-                        size={30} 
+                        size={20} 
                     />
 
                     <Icon
                         style={{ paddingLeft: 10}}
                         onPress={() => {}} 
                         name="upload" 
-                        size={30} 
+                        size={20} 
                     />
                 </View>
+
+                <Text style={styles.likes}>{this.props.post.likes} {this.props.post.likes > 1 ? 'likes' : 'like'}</Text>
             </View>
         );
     }
@@ -50,7 +63,31 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
-        alignItems: 'center', 
-        justifyContent: 'center',
+        borderColor: '#E0E0E0',
+        borderStyle: 'solid',
+        borderWidth: 1,
+        marginBottom: 10,
+    },
+    header: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        padding: 5,
+        borderBottomColor: '#e2e2e2',
+        borderBottomWidth: 1,
+    },
+    icons: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        margin: 10,
+    },
+    likes: {
+        paddingLeft: 10,
+        fontSize: 15,
+        fontWeight: 'bold',
+        margin: 10,
     },
 });
