e65e0d03bfb71de82a23f0fdde95016f12113be5
Xiaoping-Li <narci2008@gmail.com>
2019-04-16 12:14:29 -0700

    Style Search page

--- InstagramClone/src/components/screens/SearchScreen.js
+++ InstagramClone/src/components/screens/SearchScreen.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, StyleSheet, TextInput, } from 'react-native';
+import Icon from '@expo/vector-icons/Ionicons';
 
 class SearchScreen extends Component {
+    constructor() {
+        super();
+        this.state={
+            query: '',
+        };
+    }
     render() {
         return (
             <View style={styles.container}>
-                <Text>SearchScreen</Text>
+                <Icon 
+                    color="#e0e0e0"
+                    name="md-search"
+                    size={20}
+                />
+
+                <TextInput
+                    placeholder="Search"
+                    value={this.state.query}
+                    onChange={query => this.setState({query})}
+                    style={styles.input}
+                />
             </View>
         );
     }
@@ -15,9 +33,20 @@ export default SearchScreen;
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center', 
-      justifyContent: 'center',
+        flexDirection: 'row',
+        backgroundColor: '#fff',
+        alignItems: 'center', 
+        justifyContent: 'center',
+        borderWidth: 1,
+        borderColor: '#e0e0e0',
+        borderStyle: 'solid',
+        borderRadius: 10,
+        marginTop: 10,
+        padding: 5,
+    },
+    input: {
+        width: 300,
+        fontSize: 20,
+        marginLeft: 5,
     },
 });
