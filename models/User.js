79e7728bbe01dfb50e8a854ded2ca8cacc5e40a6
Xiaoping-Li <narci2008@gmail.com>
2019-04-29 16:39:29 -0700

    Add userSchema

--- models/User.js
+++ models/User.js
@@ -3,10 +3,35 @@ const Schema = mongoose.Schema;
 
 // Create User Schema
 const UserSchema = new Schema({
-
+    username: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        validate: passwordValidator,
+        msg: 'password too short, at least 4 char', 
+    },
+    createAt: {
+        type: Date,
+        default: Date.now,
+    },
+    posts: [],
+    friends: [],
 });
 
 // Create User Model
 const UserModel = mongoose.model('User', UserSchema);
 
 module.exports = UserModel;
+
+// Create a Validator for password, could provide more rules inside this funciton
+const passwordValidator = pw => {
+    return pw.length >= 4;
+};
