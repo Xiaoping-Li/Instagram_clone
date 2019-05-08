7670560bf642918eb071f8dfa50227029fd9b34b
Xiaoping-Li <narci2008@gmail.com>
2019-05-08 15:18:38 -0700

    Update GlobalStore

--- InstagramClone/GlobalStore.js
+++ InstagramClone/GlobalStore.js
@@ -2,15 +2,33 @@
 import {observable, decorate, action} from 'mobx';
 
 class GlobalStore {
-    greet = 'hello';
-    changeState = (str) => this.greet = str;
+    // greet = 'hello';
+    // changeState = (str) => this.greet = str;
+    user = {
+        username: '',
+        email: '',
+        thumbnail: '',
+    };
+    updateUser = (updated) => this.user = updated;
+
+    friends = [];
+    updateFriends = (friend) => this.friends.unshift(friend);
+
+    posts = [];
+    updatePosts = (post) => this.posts.unshift(post);
 }
 
 decorate(
     GlobalStore,
     {
-        greet: observable,
-        changeState: action,
+        // greet: observable,
+        // changeState: action,
+        user: observable,
+        friends: observable,
+        posts: observable,
+        updateUser: action,
+        updateFriends: action,
+        updatePosts: action,
     }
 );
 
