19cb9e5c6b0eff81466489751be5916cbdcbc23d
Xiaoping-Li <narci2008@gmail.com>
2019-05-16 13:49:07 -0700

    FriendsScreen done

--- InstagramClone/src/components/screens/FriendsScreen.js
+++ InstagramClone/src/components/screens/FriendsScreen.js
@@ -13,17 +13,24 @@ class FriendsScreen extends Component {
     render() {
         return (
             <View style={styles.container}>
-                {
-                    globalStore.friends.map((item, idx) => (
-                        <ListItem
-                            key={idx}
-                            leftAvatar={{ source: { uri: item.thumbnail }}} 
-                            title={item.friendName}
-                            chevronColor="#a9a9a9"
-                            bottomDivider={true}  
-                        />
-                    ))
+                {globalStore.friends.length ?
+                    <View>
+                        {
+                            globalStore.friends.map((item, idx) => (
+                                <ListItem
+                                    key={idx}
+                                    leftAvatar={{ source: { uri: item.thumbnail }}} 
+                                    title={item.friendName}
+                                    chevronColor="#a9a9a9"
+                                    bottomDivider={true}  
+                                />
+                            ))
+                        }
+                    </View>
+                    : 
+                    <View style={styles.textContainer}><Text style={styles.text}>Add some Friends</Text></View>
                 }
+                
             </View>
         );
     }
@@ -35,6 +42,15 @@ const styles = StyleSheet.create({
     container: {
       flex: 1,
       backgroundColor: '#fff', 
-      justifyContent: 'center',
+      //justifyContent: 'flex-start',
     },
+    textContainer: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center', 
+    },
+    text: {
+        fontSize: 30,
+        fontWeight: 'bold',
+    }
 });
