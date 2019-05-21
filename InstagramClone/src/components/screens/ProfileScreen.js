8cb9a122252a8b17728687d2df8a48871f8dab7f
Xiaoping-Li <narci2008@gmail.com>
2019-05-21 14:41:44 -0700

    Modify profile

--- InstagramClone/src/components/screens/ProfileScreen.js
+++ InstagramClone/src/components/screens/ProfileScreen.js
@@ -1,5 +1,13 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Button, AsyncStorage, } from 'react-native';
+import { 
+    View, 
+    Text, 
+    StyleSheet, 
+    Button, 
+    AsyncStorage,
+    Image, 
+} from 'react-native';
+import { Icon } from 'react-native-elements';
 
 import globalStore from '../../../GlobalStore';
 import { action } from 'mobx';
@@ -28,12 +36,29 @@ class ProfileScreen extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <Text>Welcome to Profiles page.</Text>
-                <Text> You could modify and overview your Profiles here</Text>
-                <Button
-                    title="Sign Out"
-                    onPress={this.signOut}
-                />
+                {globalStore.user.userID ?
+                    <View>
+                        <Icon
+                            raised
+                            name='edit'
+                            type='font-awesome'
+                            onPress={() => {}} 
+                        />
+                        <View>
+                            <Image source={{uri: globalStore.user.thumbnail}} style={styles.img}/>
+                            <Text>User Name: {globalStore.user.username}</Text>
+                            <Text>Primary Email Address: {globalStore.user.email}</Text>
+                        </View>
+                        
+                        <View>
+                            <Text onPress={this.signOut}>Sign Out</Text>
+                        </View>    
+                    </View>
+                    :
+                    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+                        <Text onPress={this.signOut} style={styles.signOut}>Sign Out</Text>
+                    </View>
+                }   
             </View>
         );
     }
@@ -48,4 +73,21 @@ const styles = StyleSheet.create({
       alignItems: 'center', 
       justifyContent: 'center',
     },
+    img: {
+        width: 100,
+        height: 100,
+        borderColor: 'gray',
+        borderRadius: 50,
+        borderWidth: 1,
+    },
+    signOut: {
+        borderColor: 'red',
+        borderWidth: 1,
+        borderRadius: 10,
+        width: 100,
+        fontSize: 20,
+        padding: 5,
+        textAlign: 'center',
+        color: 'red',
+    }
 });
