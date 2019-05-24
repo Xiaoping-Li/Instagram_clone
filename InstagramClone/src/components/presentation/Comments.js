84cd1853fda653e6b203b28f5c6fdef4f964cd4d
Xiaoping-Li <narci2008@gmail.com>
2019-05-24 11:53:47 -0700

    Style of comments component

--- InstagramClone/src/components/presentation/Comments.js
+++ InstagramClone/src/components/presentation/Comments.js
@@ -15,13 +15,14 @@ class Comments extends Component {
     // Have to use keyword "item", could not change to other, otherwise, get undefined
     renderRow = ({item}) => {
         return (
-            <View style={{width: 300}}>
-                <View style={{flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center'}}>
-                    <Image source={{uri: item.user.thumbnail}} style={{width: 15, height: 15}}/>
+            <View style={{width: 350}}>
+                <View style={{width: 350, height: 1, backgroundColor: '#D3D3D3', marginBottom: 5, marginTop: 5}}></View>
+                <View style={{flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'flex-end', marginBottom: 5}}>
+                    <Image source={{uri: item.user.thumbnail}} style={{width: 20, height: 20, borderRadius: 10, marginRight: 20,}}/>
                     <Text>{item.user.username}</Text>
                 </View>
                 <Text>{item.content}</Text>
-                <View style={{width: 300, height: 1, backgroundColor: '#D3D3D3',}}></View>
+                
             </View>
         );
     }
@@ -44,5 +45,7 @@ export default Comments;
 const styles = StyleSheet.create({
     container: {
         backgroundColor: '#fff',
+        justifyContent: 'center',
+        alignItems: 'center'
     },
 });
